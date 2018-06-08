aa603a398b30a644eece43bd1f39762c6885dcb6
Оксана <zhuk11oxana@gmail.com>
2018-06-09 01:43:20 +0700

    создание массива волшебников, генерация DOM-элементов из массива

---
+++ js/setup.js
@@ -0,0 +1,53 @@
+'use strict';
+
+var userDialog = document.querySelector('.setup');
+userDialog.classList.remove('hidden');
+
+var similarListElement = userDialog.querySelector('.setup-similar-list');
+
+var similarWizardTemplate = document.querySelector('#similar-wizard-template')
+    .content
+    .querySelector('.setup-similar-item');
+
+var getRandomTrait = function (array) {
+    return array[Math.floor(Math.random() * array.length)];
+};
+
+var makeWizardsList = function () {
+  var NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+  var LAST_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+  var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+  var EYE_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+  var WIZARDS_QUANTITY = 4;
+  var wizards = [];
+
+  for (var i = 0; i < WIZARDS_QUANTITY; i++) {
+    wizards[i] = {
+      name: getRandomTrait(NAMES) + ' ' + getRandomTrait(LAST_NAMES),
+      coatColor: getRandomTrait(COAT_COLORS),
+      eyesColor: getRandomTrait(EYE_COLORS)
+    };
+  }
+  return wizards;
+};
+
+var renderWizard = function (wizard) {
+  var wizardElement = similarWizardTemplate.cloneNode(true);
+
+  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+
+  return wizardElement;
+}
+
+var renderWizardsList = function (wizards) {
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < wizards.length; i++) {
+    fragment.appendChild(renderWizard(wizards[i]));
+  }
+  similarListElement.appendChild(fragment);
+};
+
+renderWizardsList(makeWizardsList());
+userDialog.querySelector('.setup-similar').classList.remove('hidden');
