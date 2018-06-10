45e110c7b4603786442c23703c489e8f6b7916c4
Оксана <zhuk11oxana@gmail.com>
2018-06-11 00:46:25 +0700

    вынесла константы из функции

--- js/setup.js
+++ js/setup.js
@@ -1,5 +1,11 @@
 'use strict';
 
+var NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var LAST_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var EYE_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+var WIZARDS_QUANTITY = 4;
+
 var userDialog = document.querySelector('.setup');
 userDialog.classList.remove('hidden');
 
@@ -14,13 +20,7 @@ var getRandomTrait = function (array) {
 };
 
 var makeWizardsList = function () {
-  var NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-  var LAST_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-  var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-  var EYE_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
-  var WIZARDS_QUANTITY = 4;
   var wizards = [];
-
   for (var i = 0; i < WIZARDS_QUANTITY; i++) {
     wizards[i] = {
       name: getRandomTrait(NAMES) + ' ' + getRandomTrait(LAST_NAMES),
